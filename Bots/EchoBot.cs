f39eb252b8e053c24e6d7837527d76b49788b95e
AnnaSibirtseva <71088887+AnnaSibirtseva@users.noreply.github.com>
2021-03-22 14:06:17 +0300

    Anna created this!

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -24,7 +24,22 @@ namespace Microsoft.BotBuilderSamples.Bots
                     result = NewTask.Condition;
                     CSharpCompiler.NowTask = NewTask;
                     break;
-                default:
+                case "/help":
+                    string helper = "/random – random task" + Environment.NewLine + "/code - send the code to check it" + Environment.NewLine  + "/mood - shows a picture of Kermit";
+                    await turnContext.SendActivityAsync(helper);
+                    break;
+                case "/mood":
+                    var pic = MessageFactory.Text("", "");
+                    var PhotoAttach = new Attachment()
+                    {
+                        Name = "maxresdefault.jpg",
+                        ContentType = "image/jpg",
+                        ContentUrl = MoodPhoto()
+                    };
+                    pic.Attachments = new List<Attachment>() { PhotoAttach };
+                    await turnContext.SendActivityAsync(pic);
+                    break;
+                case "/code":
                     CSharpCompiler.BuildProject(out bool error, turnContext.Activity.Text);
                     if (!error)
                     {
@@ -43,7 +58,7 @@ namespace Microsoft.BotBuilderSamples.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hello there!";
+            var welcomeText = "Hello dear friend!" + Environment.NewLine + "This bot will help you to learn C#. " + Environment.NewLine + "Call /help to see all commands";
             var reply = MessageFactory.Text(welcomeText, welcomeText);
             var PhotoAttach = new Attachment()
             {
@@ -59,6 +74,26 @@ namespace Microsoft.BotBuilderSamples.Bots
                     await turnContext.SendActivityAsync(reply, cancellationToken);
                 }
             }
+        }
+        private static string MoodPhoto()
+        {
+            List<string> AllPhohtos = new List<string>()
+            {
+                "https://pbs.twimg.com/media/EMLMeOCXYAAztWs.jpg",
+                "https://image.tmdb.org/t/p/original/rQYQByzhjG4oNU8Om1CnWUSb4yf.jpg",
+                "https://i.pinimg.com/originals/72/23/e3/7223e3778b0c05dff8e197adb8e629ae.jpg",
+                "https://pbs.twimg.com/media/CTFitQoWoAArebW.jpg:large",
+                "https://images.fandango.com/ImageRenderer/0/0/redesign/static/img/default_poster.png/0/images/masterrepository/fandango/161864/muppetsmostwanted-mv-56.jpg",
+                "https://www.thesun.co.uk/wp-content/uploads/2018/02/nintchdbpict000255319028.jpg",
+                "https://c.wallhere.com/photos/c1/62/Kermit_the_Frog_flowers_Sesame_Street_The_Muppets-1336809.jpg!d",
+                "https://www.dailydot.com/wp-content/uploads/26c/b1/06e160103c1974fa0fb3d133e5d38bcf.jpg",
+                "https://yt3.ggpht.com/a/AATXAJycOI5UU75wO5_Rz_k-FUCuILJo6PqfWAS-Yg=s900-c-k-c0xffffffff-no-rj-mo",
+                "https://i.pinimg.com/736x/fc/bf/98/fcbf98ece06d0b04dd787a7e01993923.jpg",
+                "https://www.factinate.com/wp-content/uploads/2018/06/30-32.jpg"
+            };
+            Random random = new Random();
+            int index = random.Next(AllPhohtos.Count);
+            return AllPhohtos[index];
         }
     }
 }
